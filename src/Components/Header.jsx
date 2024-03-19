b6b72b444b1420204296d9ad55d2333121423f25
Adnan Pobrklic <109630160+AdnanPobrklic@users.noreply.github.com>
2024-03-19 11:45:33 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({setShowNav}){
     }
 
     return(
-        <header className="user-select-none absolute w-full top-[-40px] px-1 opacity-load-first socials flex justify-between md:justify-end text-slate-500 text-[23px] md:text-3xl">     
+        <header className="user-select-none absolute w-full top-[-40px] px-1 opacity-load-first socials flex justify-between md:justify-end text-slate-500 text-[18px] md:text-3xl">     
             <span className="md:hidden" onClick={handleNavShow}><i className="md:hover:opacity-50 cursor-pointer fa-solid fa-bars mr-[90%]"></i></span>
             <div className="flex gap-5 items-center">
                 <a href={resume} download="adnan-pobrklic-resume-en.pdf" className="cursor-pointer md:hover:text-slate-600 md:hover:scale-x-110 transition-all">CV</a>
